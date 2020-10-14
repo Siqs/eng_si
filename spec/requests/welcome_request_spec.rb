758f1ecaf88320a4d081db656a425198589350e1
gustavoyama <oyamagu@hotmail.com>
2020-10-14 19:42:36 -0300

    fixCoverallas

--- spec/requests/welcome_request_spec.rb
+++ spec/requests/welcome_request_spec.rb
@@ -8,5 +8,26 @@ RSpec.describe "Welcomes", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+  describe "GET /new" do
+    it "returns http success" do
+      get "/welcome/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe "GET /" do
+    it "returns http success" do
+      get "/welcome"
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe "POST /index" do
+    it "valid parameters" do
+      post "/welcome/", params:{
+        clinica: {name:"bem viver",address:"paulista"}
+      }
+      expect(response).to have_http_status(302)
+    end
+  end
+
 
 end
