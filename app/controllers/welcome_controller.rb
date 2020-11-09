baca6ea629e13cc7041001e699e39697aaf31044
Siqs <luc.siqueira94@gmail.com>
2020-11-08 22:58:44 -0300

    implement add clinic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,29 @@ class WelcomeController < ApplicationController
   def index
     @clinicas = Clinica.all
   end
+
+  def new 
+    @clinicas = Clinica.new
+  end
+
+  def create
+    @clinica = Clinica.new(clinica_params)
+    if @clinica.save 
+      clin = Clinica.last
+      clin.update(name: @clinica.name, address: @clinica.address)
+  
+      redirect_to welcome_index_path
+    else 
+      render 'new'
+    end    
+  end
+
+  def show 
+    @clinicas = Clinica.find([:id])
+  end
+
+  private 
+  def clinica_params
+    params.require(:clinica).permit(:name,:address)
+  end
 end
